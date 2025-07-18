6ac7fdfe459d4543ba618943d8aaa83db3cb0c0b
1orzero <laokameng.6@gmail.com>
2025-07-18 12:32:44 +0800

    Revert "feat(logging): pretty-print JSON request/response bodies"
    This reverts commit eb2c38125bdce0e749dfdbfd32c91faa6062de74.

--- main.go
+++ main.go
@@ -2,44 +2,14 @@ package main
 
 import (
 	"bytes"
-	"compress/gzip"
-	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"strings"
 	"time"
 )
-
-// formatJSON pretty prints JSON data
-func formatJSON(data []byte) string {
-	var prettyJSON bytes.Buffer
-	if err := json.Indent(&prettyJSON, data, "", "  "); err != nil {
-		return string(data) // Return original if not valid JSON
-	}
-	return prettyJSON.String()
-}
-
-// decompressBody decompresses gzip-encoded body
-func decompressBody(body []byte, encoding string) ([]byte, error) {
-	if strings.Contains(strings.ToLower(encoding), "gzip") {
-		reader, err := gzip.NewReader(bytes.NewReader(body))
-		if err != nil {
-			return nil, err
-		}
-		defer reader.Close()
-		return io.ReadAll(reader)
-	}
-	return body, nil
-}
-
-// isJSON checks if content type is JSON
-func isJSON(contentType string) bool {
-	return strings.Contains(strings.ToLower(contentType), "application/json")
-}
 
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -63,12 +33,7 @@ func logRequest(handler http.Handler) http.Handler {
 			requestBody, _ = io.ReadAll(r.Body)
 			r.Body = io.NopCloser(bytes.NewBuffer(requestBody))
 			if len(requestBody) > 0 {
-				contentType := r.Header.Get("Content-Type")
-				if isJSON(contentType) {
-					log.Printf("Request Body (JSON):\n%s", formatJSON(requestBody))
-				} else {
-					log.Printf("Request Body: %s", string(requestBody))
-				}
+				log.Printf("Request Body: %s", string(requestBody))
 			}
 		}
 
@@ -79,7 +44,6 @@ func logRequest(handler http.Handler) http.Handler {
 		responseWriter := &responseWriter{
 			ResponseWriter: w,
 			body:           &bytes.Buffer{},
-			statusCode:     0,
 		}
 
 		// Call the original handler
@@ -103,26 +67,7 @@ func logRequest(handler http.Handler) http.Handler {
 		
 		// Log response body
 		if responseWriter.body.Len() > 0 {
-			responseBody := responseWriter.body.Bytes()
-			contentType := w.Header().Get("Content-Type")
-			contentEncoding := w.Header().Get("Content-Encoding")
-			
-			// Decompress if needed
-			if contentEncoding != "" {
-				decompressed, err := decompressBody(responseBody, contentEncoding)
-				if err == nil {
-					responseBody = decompressed
-				} else {
-					log.Printf("Failed to decompress response: %v", err)
-				}
-			}
-			
-			// Format JSON if applicable
-			if isJSON(contentType) {
-				log.Printf("Response Body (JSON):\n%s", formatJSON(responseBody))
-			} else {
-				log.Printf("Response Body: %s", string(responseBody))
-			}
+			log.Printf("Response Body: %s", responseWriter.body.String())
 		}
 		
 		log.Printf("=== END ===\n")
@@ -133,23 +78,14 @@ type responseWriter struct {
 	http.ResponseWriter
 	statusCode int
 	body       *bytes.Buffer
-	writerUsed bool
 }
 
 func (rw *responseWriter) WriteHeader(statusCode int) {
-	if rw.statusCode == 0 {
-		rw.statusCode = statusCode
-	}
+	rw.statusCode = statusCode
 	rw.ResponseWriter.WriteHeader(statusCode)
 }
 
 func (rw *responseWriter) Write(b []byte) (int, error) {
-	if !rw.writerUsed {
-		rw.writerUsed = true
-		if rw.statusCode == 0 {
-			rw.statusCode = http.StatusOK
-		}
-	}
 	rw.body.Write(b)
 	return rw.ResponseWriter.Write(b)
 }
